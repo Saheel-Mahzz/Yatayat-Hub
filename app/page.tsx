faf32e3e89db5607e72724ced970691310ef3c60
saheel <programmingwithsahil@gmail.com>
2026-07-01 01:07:51 +0545

    fix: fixes in the type issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>I am app..</div>;
+import Bookings from "@/modules/bookings";
+import React from "react";
+
+export default async function page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const search = await searchParams;
+  console.log("search page", search);
+  return <Bookings searchParams={search} />;
 }
