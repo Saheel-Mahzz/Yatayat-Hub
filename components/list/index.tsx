9b8f13f8472b44058e06a5bf801fbd04381fa7aa
saheel <programmingwithsahil@gmail.com>
2026-07-06 21:06:18 +0545

    fix: added a table list file

---
+++ components/list/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function List() {
+  return <div>I am list file...</div>;
+}
--- modules/myBookings/index.tsx
+++ modules/myBookings/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { getBookings } from "./api/getBookings";
+import List from "@/components/list";
 
 export default async function MyBookings() {
   const response = await getBookings();
   const allBooking = response?.data?.results || [];
   return (
-    <div className="max-w-5xl mx-auto">
-      List of bookings..
-      {allBooking.map((booking) => (
-        <div key={booking.booked_at}>{booking.seat_number}</div>
-      ))}
-    </div>
+    // <div className="max-w-5xl mx-auto">
+    //   List of bookings..
+    //   {allBooking.map((booking) => (
+    //     <div key={booking.booked_at}>{booking.seat_number}</div>
+    //   ))}
+    // </div>
+
+    <List />
   );
 }
