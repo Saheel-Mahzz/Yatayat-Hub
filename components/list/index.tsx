25596bf9fc221cb85b7636ca6b5e983501a011d9
saheel <programmingwithsahil@gmail.com>
2026-07-06 22:15:10 +0545

    feat: made a generic table

--- components/list/index.tsx
+++ components/list/index.tsx
@@ -1,5 +1,118 @@
-import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Column } from "@/modules/myBookings";
 
-export default function List() {
-  return <div>I am list file...</div>;
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
+
+interface IList {
+  columns: Column[];
+  rows: any[];
+}
+
+function getNestedValue(obj: any, path: any) {
+  return path?.split(".").reduce((acc, curr) => {
+    return acc[curr] !== undefined ? acc[curr] : undefined;
+  }, obj);
+}
+
+export function List({ columns, rows }: IList) {
+  return (
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          {columns.map((col, index) => (
+            <TableHead key={index}>{col?.header}</TableHead>
+          ))}
+          {/* <TableHead className="w-[100px]">Invoice</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Method</TableHead>
+          <TableHead className="text-right">Amount</TableHead> */}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {/* {rows.map((row, i) => (
+           <TableRow key={i}>
+
+           columns.map((col) => (
+              <TableCell>{getNestedValue(row, col?.accessorKey)}</TableCell>
+          ))
+            </TableRow>
+
+        ))} */}
+        {rows.map((row, i) => (
+          <TableRow key={i}>
+            {
+              /* 1. Yahan curly brace '{' start gara */
+              columns.map((col, i) => (
+                <TableCell key={i}>
+                  {getNestedValue(row, col?.accessorKey)}
+                </TableCell>
+              ))
+              /* 2. Yahan curly brace '}' close gara */
+            }
+          </TableRow>
+        ))}
+        {/* {invoices.map((invoice) => (
+          <TableRow key={invoice.invoice}>
+            <TableCell className="font-medium">{invoice.invoice}</TableCell>
+            <TableCell>{invoice.paymentStatus}</TableCell>
+            <TableCell>{invoice.paymentMethod}</TableCell>
+            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+          </TableRow>
+        ))} */}
+      </TableBody>
+    </Table>
+  );
 }
--- modules/myBookings/index.tsx
+++ modules/myBookings/index.tsx
@@ -1,10 +1,48 @@
 import React from "react";
 import { getBookings } from "./api/getBookings";
-import List from "@/components/list";
+import { List } from "@/components/list";
+export interface Column {
+  header: string;
+  accessorKey: string;
+}
 
 export default async function MyBookings() {
   const response = await getBookings();
   const allBooking = response?.data?.results || [];
+  console.log("al booking", allBooking);
+
+  const columns: Column[] = [
+    {
+      header: "User Name",
+      accessorKey: "user.first_name",
+    },
+    {
+      header: "Bus Name",
+      // accessorKey: "bus.name",//check
+      accessorKey: "trip.bus.name",
+    },
+    {
+      header: "Number Plate",
+      accessorKey: "trip.bus.number_plate",
+    },
+    {
+      header: "Seat Number",
+      accessorKey: "seat_number",
+    },
+
+    {
+      header: "Email",
+      accessorKey: "user.email",
+    },
+    {
+      header: "From Destination",
+      accessorKey: "trip.from_location",
+    },
+    {
+      header: "To Destination",
+      accessorKey: "trip.to_location",
+    },
+  ];
   return (
     // <div className="max-w-5xl mx-auto">
     //   List of bookings..
@@ -12,7 +50,8 @@ export default async function MyBookings() {
     //     <div key={booking.booked_at}>{booking.seat_number}</div>
     //   ))}
     // </div>
-
-    <List />
+    <div className="  max-w-5xl ">
+      <List columns={columns} rows={allBooking} />
+    </div>
   );
 }
