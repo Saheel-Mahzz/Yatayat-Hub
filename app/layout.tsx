de386c11725c9c161beb8aff962a0cb0831576a4
saheel <programmingwithsahil@gmail.com>
2026-08-21 08:24:04 +0545

    fix: added a suspense in the trip pagination

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
